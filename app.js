15de4d76f7962d45111d3e6c33031dd66d84e715
aemingenc <aemingenc@gmail.com>
2021-12-02 15:25:15 +0200

    js update

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ function getResult(data){
         console.log("not found");
     }
     else if(data[0].meanings.length<2){
-        content.innerHTML = ` <p>type :${data[0].meanings[0].partOfSpeech}</p>
-     <p>meaning :${data[0].meanings[0].definitions[0].definition}</p>`
+        content.innerHTML = ` <p class="type">type :${data[0].meanings[0].partOfSpeech}</p><br>
+     <p class="meaning">meaning :${data[0].meanings[0].definitions[0].definition}</p>`
 
     }
-    else{content.innerHTML = ` <p>type :${data[0].meanings[0].partOfSpeech}</p>
-      <p>meaning :${data[0].meanings[0].definitions[0].definition}</p>
-      <p>type :${data[0].meanings[1].partOfSpeech}</p>
-      <p>meaning :${data[0].meanings[1].definitions[0].definition}</p>` 
+    else{content.innerHTML = ` <p class="type">type: ${data[0].meanings[0].partOfSpeech}</p> <br>
+      <p class="meaning"><span>meaning:</span> ${data[0].meanings[0].definitions[0].definition}</p> <br>
+      <p class="type">type: ${data[0].meanings[1].partOfSpeech}</p> <br>
+      <p class="meaning"><span>meaning:</span> ${data[0].meanings[1].definitions[0].definition}</p>` 
     }
         
     
--- ui.js
+++ ui.js
@@ -1,9 +1,20 @@
-// function getResult(data){
-//     const content = document.getElementById("content");
-//     content.innerHTML = ` <p>meaning :${data[0].meanings[1].definitions [0]}</p>
-//     <p>type :${data[0].meanings[1].partOfSpeech}</p>
-//     <p>meaning :${data[0].meanings[2].definitions [0]}</p>
-//     <p>type :${data[0].meanings[2].partOfSpeech}</p>` 
-    
 
-// } 
+// class UI{
+//  getResult(data){
+//     const content = document.getElementById("content");
+//     if(data[0].meanings.length<=0){
+//         console.log("not found");
+//     }
+//     else if(data[0].meanings.length<2){
+//         content.innerHTML = ` <p>type :${data[0].meanings[0].partOfSpeech}</p>
+//      <p>meaning :${data[0].meanings[0].definitions[0].definition}</p>`
+
+//     }
+//     else{content.innerHTML = ` <p class="type">type: ${data[0].meanings[0].partOfSpeech}</p> <br>
+//       <p class="meaning"><span>meaning:</span> ${data[0].meanings[0].definitions[0].definition}</p> <br>
+//       <p class="type">type: ${data[0].meanings[1].partOfSpeech}</p> <br>
+//       <p class="meaning"><span>meaning:</span> ${data[0].meanings[1].definitions[0].definition}</p>` 
+//     }
+        
+
+// } }
